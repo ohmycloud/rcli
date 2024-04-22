37a2e0e0d9bb5a8890db973b7ce3f5d07d936942
ohmycloud <ohmycloudy@gmail.com>
2024-04-22 22:56:41 +0800

    finish csv shower

--- src/process.rs
+++ src/process.rs
@@ -4,7 +4,7 @@ use std::fs;
 
 pub fn process_csv(input_path: &str, output_path: &str) -> anyhow::Result<()> {
     let mut reader = Reader::from_path(input_path)?;
-    let mut ret = Vec::with_capacity(128);
+    let mut ret = Vec::with_capacity(256);
     for result in reader.deserialize() {
         let record: Player = result?;
         ret.push(record);
